d3a4e82bf8275080f5985bd78ca76f9bb175130d
eastsunyong <108984141+eastsunyong@users.noreply.github.com>
2022-08-08 10:11:46 +0900

    메인페이지 외관 및 기술 구현중인데 안됌 ...........

---
+++ src/components/List.jsx
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+
+
+const Tedo = styled.div`
+    border: 1px solid hotpink;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    margin-top: 50px;
+`
+
+
+const Card = styled.div`
+   border: 1px solid black;
+   display: flex;
+   flex-direction: column;
+   margin: 15px 80px 15px 15px;
+   padding: 10px;
+   border-radius: 10px;
+`
+
+const CardImag = styled.div`
+   background-color: green;
+   width: 450px;
+   height: 200px;
+`
+
+const CardBody = styled.div`
+    background-color: yellow;
+    width: 450px;
+   height: 150px;
+   padding: 10px;
+   display: flex;
+   flex-direction: column;
+`
+
+const Cen = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+
+function List() {
+
+    return (
+    
+        <Tedo>
+            <Card>
+                <CardImag>
+                </CardImag>
+                <CardBody>
+                    <Cen><h3>강남 카페</h3></Cen>
+                    <p>너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요  너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요  너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요  너무너무 이뻐요</p>
+                </CardBody>
+            </Card>
+            <Card>
+                <CardImag>
+                </CardImag>
+                <CardBody>
+                    <Cen><h3>강남 카페</h3></Cen>
+                    <p>너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요  너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요  너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요  너무너무 이뻐요</p>
+                </CardBody>
+            </Card>
+            <Card>
+                <CardImag>
+                </CardImag>
+                <CardBody>
+                    <Cen><h3>강남 카페</h3></Cen>
+                    <p>너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요  너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요  너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요  너무너무 이뻐요</p>
+                </CardBody>
+            </Card>
+            <Card>
+                <CardImag>
+                </CardImag>
+                <CardBody>
+                    <Cen><h3>강남 카페</h3></Cen>
+                    <p>너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요  너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요  너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요 너무너무 이뻐요  너무너무 이뻐요</p>
+                </CardBody>
+            </Card>
+            
+        </Tedo>
+    )
+}
+
+export default List;
+
--- src/redux/modules/users.js
+++ src/redux/modules/users.js
@@ -1,16 +1,20 @@
-//Action Value
+import { createSlice } from "@reduxjs/toolkit";
 
-// Action Creator
+const initialState = [{ title: "신사동 카페", coments: "사진 맛집", img: '이미지' },
+{ title: "강남 카페", coments: "커피가 맛있어요", img: '이미지2' }]
 
-//Initial State
-const initialState = {};
 
-// 리듀서
-const users = (state = initialState, action) => {
-  switch (action.type) {
-    default:
-      return state;
-  }
-};
+const counterSlice = createSlice({
+  name: "create",
+  initialState,
+  reducers: {
+    addNumber: (state , action) => {
+      state = [state, action.payload]
+    },
+  },
+});
 
-export default users;
+// 액션크리에이터는 컴포넌트에서 사용하기 위해 export 하고
+export const { addNumber } = counterSlice.actions;
+// reducer 는 configStore에 등록하기 위해 export default 합니다.
+export default counterSlice.reducer;
